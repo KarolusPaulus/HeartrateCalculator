ad5447b50ca6608f5bfa2becdf6d51fc1b40685b
KarolusPaulus <karosaarinenn@gmail.com>
2025-01-09 09:37:47 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('')
+  const [hr, setHr] = useState('')
+
+  const calculate = () => {
+    const lower = (220-age) * 0.65
+    const upper = (220-age) * 0.85
+    const result = `${lower}-${upper}`
+    setHr(result)
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput
+      value={age}
+      onChangeText={text => setAge(text)}
+      keyboardType='decimal-pad'
+      />
+      <Text>Limits</Text>
+      <Text>{hr}</Text>
+      <Button title='Calculate' onPress={calculate}></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+    
 const styles = StyleSheet.create({
   container: {
     flex: 1,
